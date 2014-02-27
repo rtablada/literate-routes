eacb4308aeb1ffd464256bd7503c79993b9765df
Ryan Tablada <ryan.tablada@gmail.com>
2014-02-27 02:18:29 -0600

    Testing new things...

--- src/Parser.php
+++ src/Parser.php
@@ -1,23 +1,37 @@
 <?php namespace Rtablada\LiterateRoutes;
 
+use Illuminate\Support\Str;
+
 class Parser
 {
 	protected $controller;
 
 	protected $asNamespace;
 
-	public function compileString($string, $newFile = false)
+	public function compileString($string, $newFile = true)
 	{
+		$this->resetVars();
 		$parts = preg_split('/[\n\r]{2}/', $string);
 		$return = '';
 
 		foreach ($parts as $part) {
+			$return .= $this->compileVars($part);
 			$return .= $this->compileComment($part);
 			$return .= $this->compileCodeBlock($part);
 		}
 		$return = $newFile ? "<?php\n\n" . $return : $return;
 
-		return "<?php\n\n" . $return;
+		return $return;
+	}
+
+	public function compileVars($string)
+	{
+		if (preg_match('/^@setController\(\'(.*)\',\s*\'(.*)\'\)/', $string, $matches)) {
+			$this->controller = $matches[1];
+			$this->asNamespace = $matches[2];
+		} elseif (preg_match('/^@setController\(\'(.*)\'\)/', $string, $matches)) {
+			$this->controller = $matches[1];
+		}
 	}
 
 	public function compileComment($string)
@@ -25,15 +39,39 @@ class Parser
 		$matches = array();
 
 		if (preg_match('/#(.*)/', $string, $matches)) {
+			if (!$this->controller) {
+				$this->controller = Str::studly($matches[1]);
+			}
 			return "//{$matches[1]}\n\n";
 		}
 	}
 
 	public function compileCodeBlock($string)
 	{
+		$return = null;
 		if (preg_match('/^[(\s\s)\t]+@/', $string)) {
-			return $this->compileRoutes($string);
+			$return .= $this->compileShortRoutes($string);
+			$return .= $this->compileRoutes($string);
 		}
+
+		return $return;
+	}
+
+	public function compileShortRoutes($string)
+	{
+		preg_match_all('/\n?[(\s\s)\t]+(.*)-> \'(.*)\', \'(.*)\'(;)?/', $string, $matches, PREG_SET_ORDER);
+		$return = null;
+
+		foreach ($matches as $match) {
+			$method = $match[1];
+			$uri = $match[2];
+			$uses = "{$this->controller}@{$match[3]}";
+			$as = "{$this->asNamespace}.{$match[3]}";
+
+			$return .= "Route::{$method}('{$uri}', array('uses' => {$uses}, 'as' => '{$as}'));\n";
+		}
+
+		return $return;
 	}
 
 	public function compileRoutes($string)
@@ -48,5 +86,11 @@ class Parser
 		}
 
 		return $return;
+	}
+
+	protected function resetVars()
+	{
+		$this->controller = null;
+		$this->asNamespace = null;
 	}
 }
